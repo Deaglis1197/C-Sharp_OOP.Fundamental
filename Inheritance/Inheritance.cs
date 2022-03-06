ac06ca0a73d0c3f537b3fea3e7b35f6f83c4c837
Enes Gündüz <enesg1997@gmail.com>
2022-03-07 01:00:33 +0300

    Inheritance
    Inheritance Created

---
+++ Inheritance/Inheritance.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Inheritance
+{
+    //Base class
+    class Person
+    {
+        public string _name;
+        public string _surname;
+        public int _age;
+        public void PersonData(string name,string surname,int age)
+        {
+            _name=name;
+            _surname=surname;
+            _age=age;
+        }
+        public void PersonDataWriter()
+        {
+            Console.WriteLine("Name: " + _name /*or base.name*/);
+            Console.WriteLine("Surname: " + _surname);
+            Console.WriteLine("Age: " + _age);
+        }
+
+    }
+    //Inheriting Person class using:
+    class Student : Person
+    {
+        float _studentGrade;
+        int _classYear;
+        int _studentNumber;
+        public void StudentData(float studentGrade,int classYear,int studentNumber)
+        {
+            _studentGrade=studentGrade;
+            _classYear=classYear;
+            _studentNumber=studentNumber;
+        }
+        public void StudentDataWriter()
+        {
+            //as seen bellow , sub class (student) can use super class data.
+            Console.WriteLine("Name: " + _name /*or base.name*/);
+            Console.WriteLine("Surname: " + _surname);
+            Console.WriteLine("Age: " + _age);
+            Console.WriteLine("Student Grade: "+_studentGrade);
+            Console.WriteLine("Class Year: "+_classYear);
+            Console.WriteLine("Student Number: " +_studentNumber);
+
+        }
+
+    }
+    //Inheriting Person class another using:
+    class Teacher : Person
+    {
+        int teacherNumber;
+        string department;
+    }
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            var obj=new Student();
+            obj.PersonData("Enes","Gunduz",24);
+            obj.StudentData(3.13f, 4,209);
+            Console.WriteLine("Student Class Writer:\n--------------------");
+            obj.StudentDataWriter();
+            Console.WriteLine("\nPerson Class Writer:\n--------------------");
+            obj.PersonDataWriter();
+        }
+    }
+}
---
+++ Inheritance/README.md
@@ -0,0 +1,17 @@
+# Inheritance
+Inheritance is called the ability of existing a class to depend on another class and use its properties. These properties are: Instance variables and methods.
+##Important terminology:
+-Super Class: The class whose features are inherited is known as super class(or a base class or a parent class).
+-Sub Class: The class that inherits the other class is known as subclass(or a derived class, extended class, or child class). The subclass can add its own fields and methods in addition to the superclass fields and methods.
+-Reusability: Inheritance supports the concept of “reusability”, i.e. when we want to create a new class and there is already a class that includes some of the code that we want, we can derive our new class from the existing class. By doing this, we are reusing the fields and methods of the existing class.
+
+##Inheritance Types:
+-Single Inheritance:
+In single Inheritance, subclass inherit only one superclass.
+![image description](github.com/Deaglis1197/C-Sharp_OOP.Fundamental/tree/main/Inheritance/diagrams/singleheritance.png)
+-Hierarchical Inheritance:
+In hierarchical Inheritance, superclass inherit to one or more subclass.
+![image description](github.com/Deaglis1197/C-Sharp_OOP.Fundamental/tree/main/Inheritance/diagrams/hierarchicalheritance.png)
+-Another Inheritance:
+Another programing language you can create multiple superclass or subclass, unfortunately this is not defined in C#
+
