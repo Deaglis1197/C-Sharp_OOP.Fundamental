188d6e89c2740e5bb2e35e78f4525637358cb0aa
Enes Gündüz <enesg1997@gmail.com>
2022-03-09 16:00:04 +0300

    Abstraction And Encapsulation

---
+++ Abstraction/Abstraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class AbstractClass
+    {
+        protected int _data;
+        public abstract void method();
+    }
+    class SubClass : AbstractClass
+    {
+        public SubClass(int data)
+        {
+            _data=data;
+        }
+        public override void method()
+        {
+            Console.WriteLine("Data : "+ _data);
+        }
+    }
+    class Abstraction
+    {
+        static void Main(string[] args)
+        {
+            //var obj=new AbstractClass();
+            //obj.method(); this is illegal, cant create new object from any Abstract, can only create Subclass of Abstract
+            var obj=new SubClass(5);
+            obj.method();
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class EncapsulationClass
+    {
+        public int data {get;/*set;*/} //this is shortcut way for get set in c#, can defined set class 
+        protected string data2;
+        private float data3;
+        public EncapsulationClass()
+        {
+            data=75;
+        }
+        public float Data3
+        {
+            get
+            {
+            return data3;
+            }
+            set
+            {
+            data3=value;
+            }
+        }
+
+    }
+    class EncapsulationClassInherit:EncapsulationClass
+    {
+        public string Data2
+        {
+            get
+            {
+                return data2;
+            }
+            set
+            {
+                data2=value;
+            }
+        }
+    }
+    class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            var obj=new EncapsulationClass();
+            //obj.data3 this command is illegal, can only access with public set or get methods.
+            obj.Data3=5.5f;
+            Console.WriteLine("Data 3: " + obj.Data3);
+            //obj.data2 this illegal command, can only access after create public set or get methods in base class or with subclass public set or get methods.
+            var obj2=new EncapsulationClassInherit();
+            obj2.Data2="Test Data";
+            Console.WriteLine("Data 2: "+obj2.Data2);
+            //also can access data with setter and getter
+            //obj2.data=5; this is illegal, set methods not defined in class.
+            Console.WriteLine("Data : "+obj.data);            
+            
+        }
+    }
+}
