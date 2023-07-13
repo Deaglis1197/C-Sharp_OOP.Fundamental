bad408d57391e5e1f9f03ff08e30208e660fdaf1
Enes Gündüz <56505397+Deaglis1197@users.noreply.github.com>
2023-07-13 14:46:47 +0300

    Update Abstraction.cs

--- Abstraction/Abstraction.cs
+++ Abstraction/Abstraction.cs
@@ -5,7 +5,7 @@ namespace Abstraction
     abstract class AbstractClass
     {
         protected int _data;
-        public abstract void method();
+        public abstract void Method();
     }
     class SubClass : AbstractClass
     {
@@ -13,7 +13,7 @@ namespace Abstraction
         {
             _data=data;
         }
-        public override void method()
+        public override void Method()
         {
             Console.WriteLine("Data : "+ _data);
         }
@@ -25,7 +25,7 @@ namespace Abstraction
             //var obj=new AbstractClass();
             //obj.method(); this is illegal, cant create new object from any Abstract, can only create Subclass of Abstract
             var obj=new SubClass(5);
-            obj.method();
+            obj.Method();
         }
     }
 }
